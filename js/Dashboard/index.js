ce01790d7d86b94cb664a57bff8710c46b3d530d
Janis Cardenas <hope.sandoval58@gmail.com>
2023-12-17 06:00:36 +0800

    3 indicadores en grafica de barra

--- js/Dashboard/index.js
+++ js/Dashboard/index.js
@@ -1,4 +1,4 @@
-const ctx = document.getElementById("ctx");
+const ctx = document.getElementById("ctx").getContext("2d");
 
 const pintarGrafica= () => {
     new Chart(ctx, {
@@ -8,9 +8,21 @@ const pintarGrafica= () => {
           datasets: [
             {
               label: "Unemployment, total (% of total labor force)",
-              data: [3.31, 6.49, 9.46, 3.61, 7.33, 5.21], // eje y
+              data: [3.31, 6.49, 9.46, 3.61, 7.33, 5.21], // eje y (Unemployment)
               borderWidth: 1,
-              backgroundColor: ["#ff6384", "#36a2eb", "yellow", "green"],
+              backgroundColor: ["#36a2eb", "#36a2eb", "#36a2eb", "#36a2eb"],
+            },
+            {
+                label: "Urban Population (% of total)",
+                data: [81.3, 92.35, 87.56, 83.08, 35.87, 81.75], // eje y (Urban Population)
+                borderWidth: 1,
+                backgroundColor: ["#1F4E79", "#1F4E79", "#1F4E79", "#1F4E79", "#1F4E79", "#1F4E79"],
+              },
+              {
+                label: "Immigrants (% of total population)",
+                data: [4.9, 4.8, .3, 14.05, .4, 21.8],
+                borderWidth: 1,
+                backgroundColor: ["#4CAF50", "#4CAF50", "#4CAF50", "#4CAF50", "#4CAF50", "#4CAF50"],
             },
           ],
         },
