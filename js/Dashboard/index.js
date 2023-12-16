cf3c64427a6766d0b929f2351390177598381327
Janis Cardenas <hope.sandoval58@gmail.com>
2023-12-17 05:00:47 +0800

    Template grafica de prueba

--- js/Dashboard/index.js
+++ js/Dashboard/index.js
@@ -1,1 +1,27 @@
-console.log("Hola")
+const ctx = document.getElementById("ctx");
+
+const pintarGrafica= () => {
+    new Chart(ctx, {
+        type: "bar", // line, pie, bar, radar, doughnut
+        data: {
+          labels: ["Mexico", "Argentina", "Brasil", "USA", "India", "Canada"], // eje x
+          datasets: [
+            {
+              label: "Unemployment, total (% of total labor force)",
+              data: [3.31, 6.49, 9.46, 3.61, 7.33, 5.21], // eje y
+              borderWidth: 1,
+              backgroundColor: ["#ff6384", "#36a2eb", "yellow", "green"],
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+    };
+    
+window.addEventListener("load", pintarGrafica);
