d47421b540b96d645f0aa4e4376a9edc7e27df71
Janis Cardenas <hope.sandoval58@gmail.com>
2023-12-23 07:54:06 +0800

    Funcionalidad a select country

--- js/Dashboard/index.js
+++ js/Dashboard/index.js
@@ -1,60 +1,79 @@
+const Country = document.getElementById("selectCountry");
 const ctx = document.getElementById("ctx").getContext("2d");
 
-const obtenerIndicadores = async () => {
-      const response = await fetch("https://api.worldbank.org/v2/country/all/indicator/SL.UEM.TOTL.ZS?format=json&per_page=16758");
+const fetchData = async () => {
+  try {
+    const response = await fetch('https://api.worldbank.org/v2/country?format=json&per_page=500');
+    const data = await response.json();
+
+    const selectCountry = document.getElementById('selectCountry');
+    data[1].forEach(country => {
+      const option = document.createElement('option'); // crear dinámicamente elementos de opción y agregarlos a <select> en html
+      option.value = country.id;
+      option.textContent = country.name;
+      selectCountry.appendChild(option);
+    });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+const fetchUrbanData = async () => {
+  try {
+      const selectedCountry = Country.value;
+      const response = await fetch(`https://api.worldbank.org/v2/country/${selectedCountry}/indicator/SP.URB.TOTL.IN.ZS?format=json&per_page=16758`);
       const json = await response.json();
-      console.log(json[1]);
 
-      pintarGrafica(json[1])
-    };
+      drawChart(json[1]);
+  } catch (error) {
+      console.error('Error fetching urban data:', error);
+  }
+};
 
-const pintarGrafica = (indicadoresArray) => {
-      console.log(indicadoresArray) 
-      
-      const paises = indicadoresArray.map((pais) => {
-        return pais.country.value;
-      });
-      const indicadores = indicadoresArray.map((pais) => {
-        return pais.value;
-      });
 
-      
-//      const paises = ["Mexico", "Argentina", "Brasil", "USA", "India", "Canada"]
-//      const indicadores = [3.31, 6.49, 9.46, 3.61, 7.33, 5.21]
 
-    new Chart(ctx, {
-        type: "bar", // line, pie, bar, radar, doughnut
-        data: {
-          labels: paises, // eje x
-          datasets: [
-            {
-              label: "Unemployment, total (% of total labor force)",
-              data: indicadores, // eje y (Unemployment)
-              borderWidth: 1,
-              backgroundColor: ["#36a2eb", "#36a2eb", "#36a2eb", "#36a2eb"],
-            },
-            {
-                label: "Urban Population (% of total)",
-                data: [81.3, 92.35, 87.56, 83.08, 35.87, 81.75], // eje y (Urban Population)
-                borderWidth: 1,
-                backgroundColor: ["#1F4E79", "#1F4E79", "#1F4E79", "#1F4E79", "#1F4E79", "#1F4E79"],
-              },
-              {
-                label: "Immigrants (% of total population)",
-                data: [4.9, 4.8, .3, 14.05, .4, 21.8],
-                borderWidth: 1,
-                backgroundColor: ["#4CAF50", "#4CAF50", "#4CAF50", "#4CAF50", "#4CAF50", "#4CAF50"],
-            },
-          ],
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
+const drawChart = (urbanValuesArray) => {
+  console.log(urbanValuesArray);
+
+  const sortedData = urbanValuesArray
+  .sort((a, b) => a.date - b.date);
+
+const years = sortedData.map((pais) => pais.date);
+const percentages = sortedData.map((pais) => pais.value);
+
+
+//  const years = [2015, 2016, 2017, 2018, 2019]
+//    const percentages = [20, 25, 27, 29, 30]
+
+  const existingChart = Chart.getChart("ctx"); // Obtiene grafico actual
+
+  if (existingChart) {
+    existingChart.destroy(); // Si hay un gráfico existente, destrúyelo
+}
+
+
+  new Chart(ctx, {
+    type: "bar", // line, pie, bar, radar, doughnut
+    data: {
+      labels: years, //anio eje x
+      datasets: [
+        {
+            label: "Urban Population (% of total)",
+            data: percentages, //indicadores eje y (Urban Population)
+            borderWidth: 1,
+            backgroundColor: ["#1F4E79", "#1F4E79", "#1F4E79", "#1F4E79", "#1F4E79", "#1F4E79"],
           },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
         },
-      });
-    };
-    
-window.addEventListener("load", obtenerIndicadores);
+      },
+    },
+  });
+}
+
+window.addEventListener("load", fetchData);
+Country.addEventListener("change", fetchUrbanData);
